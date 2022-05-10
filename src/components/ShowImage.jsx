fa9a5172b8c3e78d51d68eb7c06dc5ee2bfc6cd2
leafarpontes <rafapontes2008@hotmail.com>
2022-05-10 00:18:50 -0300

    Consertado CSS para centralizar imagem

--- src/components/ShowImage.jsx
+++ src/components/ShowImage.jsx
@@ -4,8 +4,8 @@ export function ShowImage({ handleImage }) {
     handleImage && (
       <div className="is-flex is-justify-content-center mt-4 mb-5 is-flex-direction-column is-align-items-center">
       <p className="mb-3">Foto a ser enviada:</p>
-      <div className="img-container">
-        <img src={handleImage} alt="" />
+      <div className="is-flex is-justify-content-center px-6">
+        <img className="show-img" src={handleImage} alt="" />
       </div>
     </div>
     )
